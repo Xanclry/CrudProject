ee28cfce9b5f5e1b38b9beaa64bd308d08866706
Your Name <fed_rekish@hotmail.com>
2020-06-15 16:16:59 +0300

    Initial commit

---
+++ src/main/java/model/User.java
@@ -0,0 +1,43 @@
+package model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Objects;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String role;
+
+    private String email;
+
+    private String password;
+
+    public User(String role, String email, String password) {
+        this.role = role;
+        this.email = email;
+        this.password = password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(role, user.role) &&
+                Objects.equals(email, user.email) &&
+                Objects.equals(password, user.password);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(role, email, password);
+    }
+}
+
